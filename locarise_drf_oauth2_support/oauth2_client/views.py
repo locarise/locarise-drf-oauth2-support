ad9b9c6931a590a97781820fc05450d66a1d68f8
Giang Manh <manhg@users.noreply.github.com>
2016-05-10 11:20:11 +0900

    Replace django-social-auth reference by new python-social-auth reference
    [Fix #118541563]

--- locarise_drf_oauth2_support/oauth2_client/views.py
+++ locarise_drf_oauth2_support/oauth2_client/views.py
@@ -7,8 +7,8 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status, parsers, renderers
 
-from social_auth.backends import get_backend
-from social_auth.exceptions import WrongBackend
+from social.backends.utils import get_backend
+from social.exceptions import WrongBackend
 
 from .serializers import AuthTokenSerializer, AccessTokenSerializer
 
@@ -25,7 +25,7 @@ class ObtainAuthToken(APIView):
     `Authorization` HTTP header. The key should be prefixed by the string
     literal "Token", with whitespace separating the two strings. For example:
 
-        Authorization: Token 9944b09199c62bcf9418ad846dd0e4bbdfc6ee4b
+        Authorization: Token abc123
 
     Unauthenticated responses that are denied permission will result in an
     `HTTP 401` Unauthorized response with an appropriate WWW-Authenticate
@@ -62,9 +62,9 @@ class OAuthObtainAuthToken(APIView):
     model = Token
 
     def post(self, request, backend, *args, **kwargs):
-        backend = request.social_auth_backend = get_backend(backend, request,
+        backend = request.social_backend = get_backend(backend, request,
                                                             request.path)
-        if request.social_auth_backend is None:
+        if request.social_backend is None:
             raise WrongBackend(backend)
         serializer = self.serializer_class(data=request.DATA, backend=backend)
         if serializer.is_valid():
