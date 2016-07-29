8b78106c2db5fa816aa9e7bd59ae120290be6e06
Charles V. Dulac <charles.vdulac@gmail.com>
2016-07-29 16:05:12 +0900

    Remove Django 1.10 deprecation warnings

--- locarise_drf_oauth2_support/oauth2_client/urls.py
+++ locarise_drf_oauth2_support/oauth2_client/urls.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
-from django.conf.urls import patterns, url, include
+from django.conf.urls import url, include
+from django.contrib.auth.views import login, logout
 
 from .views import ObtainAuthToken, OAuthObtainAuthToken
 
@@ -9,19 +10,26 @@ template_name = {'template_name': 'oauth2_client/login.html'}
 
 next_page = {'next_page': '/'}
 
-auth_patterns = patterns('django.contrib.auth.views',  # noqa
+
+auth_patterns = [
     # API Views
-    url(r'^token', ObtainAuthToken.as_view(), name='token'),
-    url(r'^oauth2/(?P<backend_name>[^/]+)/token$',
+    url(
+        r'^token',
+        ObtainAuthToken.as_view(),
+        name='token'
+    ),
+    url(
+        r'^oauth2/(?P<backend_name>[^/]+)/token$',
         OAuthObtainAuthToken.as_view(),
         name='oauth2-token'
     ),
-    # Browsable API Views
-    url(r'^login$', 'login', template_name, name='login'),
-    url(r'^logout$', 'logout', next_page, name='logout'),
-)
 
-urlpatterns = patterns('',   # noqa
+    # Browsable API Views
+    url(r'^login$', login, template_name, name='login'),
+    url(r'^logout$', logout, next_page, name='logout'),
+]
+
+urlpatterns = [
     url('', include('social.apps.django_app.urls', namespace='social')),
     url(r'^auth/', include(auth_patterns, namespace='rest_framework')),
-)
+]
--- locarise_drf_oauth2_support/users/urls.py
+++ locarise_drf_oauth2_support/users/urls.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 
 from .views import CurrentUserView
 
 
-urlpatterns = patterns('',   # noqa
+urlpatterns = [
     url(r'^current-user$', CurrentUserView.as_view(), name='current-user'),
     url(r'^me$', CurrentUserView.as_view(), name='me')
-)
+]
