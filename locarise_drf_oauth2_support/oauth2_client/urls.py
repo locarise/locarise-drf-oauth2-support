6410d017d0a19d5e4ac632c6a637419cb09dc017
Sylvain Pascou <sylvain@pascou.net>
2020-01-14 17:20:22 +0900

    (0.4.0) support django==2.1 and drop django<=1.11

--- locarise_drf_oauth2_support/oauth2_client/urls.py
+++ locarise_drf_oauth2_support/oauth2_client/urls.py
@@ -1,35 +1,31 @@
 # -*- coding: utf-8 -*-
 
-from django.conf.urls import url, include
-from django.contrib.auth.views import login, logout
+from django.conf.urls import include, url
+from django.contrib.auth.views import LoginView, LogoutView
 
-from .views import ObtainAuthToken, OAuthObtainAuthToken
+from .views import OAuthObtainAuthToken, ObtainAuthToken
 
+template_name = "oauth2_client/login.html"
 
-template_name = {'template_name': 'oauth2_client/login.html'}
-
-next_page = {'next_page': '/'}
-
+next_page = "/"
 
 auth_patterns = [
     # API Views
+    url(r"^token", ObtainAuthToken.as_view(), name="token"),
     url(
-        r'^token',
-        ObtainAuthToken.as_view(),
-        name='token'
-    ),
-    url(
-        r'^oauth2/(?P<backend_name>[^/]+)/token$',
+        r"^oauth2/(?P<backend_name>[^/]+)/token$",
         OAuthObtainAuthToken.as_view(),
-        name='oauth2-token'
+        name="oauth2-token",
     ),
-
     # Browsable API Views
-    url(r'^login$', login, template_name, name='login'),
-    url(r'^logout$', logout, next_page, name='logout'),
+    url(r"^login$", LoginView.as_view(template_name=template_name), name="login"),
+    url(r"^logout$", LogoutView.as_view(template_name=next_page), name="logout"),
 ]
 
 urlpatterns = [
-    url('', include('social_django.urls', namespace='social')),
-    url(r'^auth/', include(auth_patterns, namespace='rest_framework')),
+    url("", include(("social_django.urls", "rest_framework"), namespace="social")),
+    url(
+        r"^auth/",
+        include((auth_patterns, "rest_framework"), namespace="rest_framework"),
+    ),
 ]
