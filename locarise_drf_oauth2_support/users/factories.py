6ec65d382493c365260e19155d1006b95d31835d
Sylvain Pascou <sylvain@pascou.net>
2020-09-11 16:07:29 +0900

    Fix DjangoModelFactory import

--- locarise_drf_oauth2_support/users/factories.py
+++ locarise_drf_oauth2_support/users/factories.py
@@ -1,4 +1,5 @@
 from datetime import timedelta
+
 from django.utils import timezone
 
 from locarise_drf_oauth2_support.users.models import User
@@ -6,23 +7,25 @@ from locarise_drf_oauth2_support.users.models import User
 try:
     import factory
 
-    class UserF(factory.DjangoModelFactory):
-        first_name = factory.Sequence(lambda n: 'first_name%s' % n)
-        last_name = factory.Sequence(lambda n: 'last_name%s' % n)
-        email = factory.Sequence(lambda n: 'email%s@example.com' % n)
+    class UserF(factory.django.DjangoModelFactory):
+        first_name = factory.Sequence(lambda n: "first_name%s" % n)
+        last_name = factory.Sequence(lambda n: "last_name%s" % n)
+        email = factory.Sequence(lambda n: "email%s@example.com" % n)
         is_staff = False
         is_active = True
         is_superuser = False
         last_login = timezone.now() - timedelta(days=2)
-        password = factory.PostGenerationMethodCall('set_password', 'pass')
-        organizations = [{
-            'uid': '6tbgzDKyZYLCMzDarN7ga8',
-            'name': 'Organization Demo',
-            'role': 'organization-manager',
-            'is_active': True,
-            'is_demo': False,
-            'state': 'client'
-        }]
+        password = factory.PostGenerationMethodCall("set_password", "pass")
+        organizations = [
+            {
+                "uid": "6tbgzDKyZYLCMzDarN7ga8",
+                "name": "Organization Demo",
+                "role": "organization-manager",
+                "is_active": True,
+                "is_demo": False,
+                "state": "client",
+            }
+        ]
 
         class Meta:
             model = User
